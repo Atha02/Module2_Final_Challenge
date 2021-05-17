5351352006a55ec086eb6e3e70f6bfe62bf50635
Athanasios Chatzikonstantinou <atha2015@icloud.com>
2021-05-17 09:57:49 +0200

    Initial Commit

---
+++ CWC_Quote_App/CWC_Quote_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_Quote_AppApp.swift
+//  CWC_Quote_App
+//
+//  Created by Athanasios Chatzikonstantinou on 17.05.21.
+//
+
+import SwiftUI
+
+@main
+struct CWC_Quote_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWC_Quote_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CWC_Quote_App
+//
+//  Created by Athanasios Chatzikonstantinou on 17.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
